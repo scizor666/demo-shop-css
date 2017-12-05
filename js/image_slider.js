710d72f33b1e909aef959a0e1da6860435c3768a
Alexander Teplyashin <scizor46@gmail.com>
2017-12-04 17:38:12 -0800

    Add: image slider zoom functionality

--- js/image_slider.js
+++ js/image_slider.js
@@ -15,31 +15,76 @@ class ImageSlider extends HTMLElement {
     }
 
     style() {
-        return '<style>#shadowroot {position: relative} ' +
-            '#canvas { width: 100%; display: block;} ' +
-            `#left-arrow, #right-arrow {
+        return `<style>
+            #wrapper {
+                position: relative;}
+            #image-canvas { 
+                width: 100%; 
+                display: block;} 
+            #left-arrow, #right-arrow {
                 position: absolute; 
-                height: 100%; 
+                height: 50px; 
                 width: 40px;
-                top: 0; 
-                background: url(${this.arrow()}) no-repeat 100% transparent; }` +
-            `#left-arrow { 
+                top: calc(50% - 25px); 
+                cursor: pointer;
+                background: url(${this.arrow()}) no-repeat 100% transparent; }
+            #left-arrow { 
                 left: 0; 
-                padding-left: 15px;}` +
-            `#right-arrow { 
+                padding-left: 15px;}
+            #right-arrow { 
                 transform: scaleX(-1);  
                 right: 0; 
-                padding-right: 15px;}` +
-            '</style>';
+                padding-right: 15px;}
+            #hover-area {
+                visibility: hidden;
+                position: absolute;
+                width: 22%;
+                height: 40%;
+                background: #ffffff;
+                opacity: .5;
+                left: 0;
+                top: 0;
+                border: 3px solid rgba(255,255,0,0.97);
+                box-sizing: border-box;
+                -webkit-transition: all 100ms linear;
+                transition: all 100ms linear;}
+            #zoom-wrapper {
+                position: absolute;               
+                box-sizing: border-box;
+                border: 3px solid rgba(255,255,0,0.97);
+                visibility: hidden;
+                left: calc(100% + 6px);
+                top: 0;
+                background: black;
+                height: 90%;
+                width: 50%;
+                z-index: 1;
+                }
+            #zoom-canvas {
+                width: 100%;
+                height: 100%;
+                display: block;
+            }
+        </style>`;
     }
 
     html() {
-        return '<span id="left-arrow"></span><canvas id="canvas"></canvas><span id="right-arrow"></span>';
+        return `<div id="wrapper">
+                    <canvas id="image-canvas"></canvas>
+                    <div id="hover-area"></div>                 
+                </div>
+                <span id="left-arrow"></span>
+                <span id="right-arrow"></span>
+                <div id="zoom-wrapper">
+                    <canvas id="zoom-canvas"></canvas>
+                </div>`;
     }
 
     connectedCallback() {
         this.state.images = JSON.parse(this.getAttribute('images'));
-        this.state.canvas = this.shadow.getElementById('canvas');
+        this.state.canvas = this.shadow.getElementById('image-canvas');
+
+        this.registerCanvasHoverCallbacks();
 
         this.renderImage(this.state.images[this.state.currentIndex], 1, 0);
 
@@ -57,6 +102,57 @@ class ImageSlider extends HTMLElement {
 
     }
 
+    registerCanvasHoverCallbacks() {
+        const hoverArea = this.shadow.getElementById('hover-area');
+
+        this.state.canvas.addEventListener('mousemove', e => {
+            const newOffsetX = e.offsetX - hoverArea.offsetWidth / 2;
+            const newOffsetY = e.offsetY - hoverArea.offsetHeight / 2;
+            this.adjustHoverDisplay(newOffsetX, newOffsetY);
+            this.adjustZoomDisplay(newOffsetX, newOffsetY, hoverArea.offsetWidth, hoverArea.offsetHeight);
+        });
+
+        hoverArea.addEventListener('mousemove', e => {
+            const newOffsetX = parseInt(hoverArea.style.left) + e.movementX;
+            const newOffsetY = parseInt(hoverArea.style.top) + e.movementY;
+            this.adjustHoverDisplay(newOffsetX, newOffsetY);
+            this.adjustZoomDisplay(newOffsetX, newOffsetY, hoverArea.offsetWidth, hoverArea.offsetHeight);
+        });
+
+        this.shadow.getElementById('wrapper').addEventListener('mouseleave',
+            () => this.shadow.getElementById('hover-area').style.visibility = 'hidden')
+    }
+
+    adjustHoverDisplay(newOffsetX, newOffsetY) {
+        const hoverArea = this.shadow.getElementById('hover-area');
+        if (newOffsetX < 0 || newOffsetY < 0
+            || (this.state.canvas.offsetWidth - newOffsetX - hoverArea.offsetWidth) < 0
+            || (this.state.canvas.offsetHeight - newOffsetY - hoverArea.offsetHeight) < 0) {
+            hoverArea.style.visibility = 'hidden';
+        } else {
+            hoverArea.style.left = newOffsetX + 'px';
+            hoverArea.style.top = newOffsetY + 'px';
+            hoverArea.style.visibility = 'visible';
+        }
+    }
+
+    adjustZoomDisplay(sx, sy, sw, sh) {
+        const zoomArea = this.shadow.getElementById('zoom-wrapper');
+        if (this.shadow.getElementById('hover-area').style.visibility === 'visible') {
+            const zoomCanvas = zoomArea.firstElementChild;
+            const scaleX = this.state.canvas.width / this.state.canvas.offsetWidth;
+            const scaleY = this.state.canvas.width / this.state.canvas.offsetWidth;
+            zoomCanvas.width = sw * scaleX;
+            zoomCanvas.height = sh * scaleY;
+            zoomCanvas.getContext('2d').drawImage(this.state.canvas,
+                sx * scaleX, sy * scaleY, zoomCanvas.width, zoomCanvas.height,
+                0, 0, zoomCanvas.width, zoomCanvas.height);
+            zoomArea.style.visibility = 'visible';
+        } else {
+            zoomArea.style.visibility = 'hidden';
+        }
+    }
+
     renderImage(imgSource, opacity = .5, delay = 100) {
         const canvas = this.state.canvas;
         const img = new Image();
@@ -65,27 +161,28 @@ class ImageSlider extends HTMLElement {
                 canvas.width = img.width;
                 canvas.height = img.height;
             }
-            this.drawImageWithAnimation(canvas, img, opacity, delay);
+            this.drawImageWithAnimation(img, opacity, delay);
         };
         img.src = imgSource;
         return canvas
     }
 
-    drawImageWithAnimation(canvas, image, opacity, delay) {
-        const context = canvas.getContext('2d');
-        const offset = 50;
-        const waterMark = "DEMO SHOP";
+    drawImageWithAnimation(image, opacity, delay) {
+        const context = this.state.canvas.getContext('2d');
         setTimeout(() => {
             context.globalAlpha = opacity;
 
             context.drawImage(image, 0, 0);
             if (opacity < 1) {
-                this.drawImageWithAnimation(canvas, image, Math.min(1, opacity + .3), delay);
+                this.drawImageWithAnimation(image, Math.min(1, opacity + .3), delay);
             }
             context.globalAlpha = .5;
             context.fillStyle = 'white';
-            context.font = "30px \"PT Sans\", sans-serif";
-            context.fillText(waterMark, canvas.width - context.measureText(waterMark).width - offset, offset);
+            context.font = '30px "PT Sans", sans-serif';
+            const waterMark = 'DEMO SHOP';
+            const offsetY = 50;
+            const offsetX = this.state.canvas.width - context.measureText(waterMark).width - offsetY;
+            context.fillText(waterMark, offsetX, offsetY);
         }, delay);
 
     }
