74bbc2f2345f852d92bcac91afe3f32918012754
Alexander Teplyashin <scizor46@gmail.com>
2017-12-06 22:57:16 -0800

    Add: more styling + some handlers

--- js/review_adder.js
+++ js/review_adder.js
@@ -2,12 +2,18 @@ class ReviewAdder extends HTMLElement {
 
     constructor() {
         super();
+        this.placeholders = {
+            months: ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'],
+            imageNotSelected: 'Image not selected',
+            imageSelected: 'Image selected',
+            name: 'Your name',
+            preview: 'Start typing and your review text will appear here...',
+            submit: "Your review has been added!"
+        };
         this.shadow = this.attachShadow({mode: 'open'});
         this.shadow.innerHTML += this.style();
         this.shadow.innerHTML += this.html();
-        this.state = {
-            currentIndex: 0
-        };
     };
 
     style() {
@@ -66,18 +72,29 @@ class ReviewAdder extends HTMLElement {
                 padding: 15px;
                 position: relative;
                 font-size: 14px;
-                
+            }
+            
+            #preview-head {
+                display: flex;
+                justify-content: space-between;
             }
             
             #preview-signature {
                 margin-bottom: 20px;
             }
             
+            i {
+                font-style: italic;
+                color: #FFCC00;
+            }
+            
             #preview-avatar {
                 width: 75px;
+                height: 75px;
                 display: inline-block;
                 margin-right: 20px;
                 vertical-align: top;
+                border: 1px solid #FFCC00;
             }
             
             #preview-text {
@@ -125,6 +142,35 @@ class ReviewAdder extends HTMLElement {
                 margin-right: 20px;
             }
             
+            #review-upload {
+                background: #ffffff;
+                display: flex;
+                justify-content: flex-end;
+                height: 100%;
+                padding: 10px 10px 10px 0;
+                max-height: 95px;
+                border-radius: 15px;
+            }
+            
+            #review-uploadText {
+                color: #000000;
+                font-size: 14px;
+                font-style: italic;
+                opacity: 0.5;
+                margin: auto auto;
+            }
+            
+            #review-uploadText.uploaded {
+                opacity: 1;
+            }
+            
+            #review-upload img {
+                width: 75px;
+                height: 75px;
+                border-radius: 15px;
+                cursor: pointer;
+            }   
+            
             #review-mainWrapper {
                 width: 100%; 
                 display: flex;
@@ -134,7 +180,8 @@ class ReviewAdder extends HTMLElement {
             
             textarea {
                 border: 5px solid transparent;
-                border-radius: 5px;	
+                border-radius: 5px;
+                padding: 10px;	
                 height: 180px;
                 width: 100%;     
                 color: #000000;
@@ -159,10 +206,10 @@ class ReviewAdder extends HTMLElement {
                 border-radius: 5px;	
                 border: 5px solid transparent;
                 margin-bottom: 20px;
-                font-size: 14px;	
-                font-style: italic;	
+                font-size: 14px;		
                 line-height: 19px;
                 outline: none;
+                padding: 0 10px; 
             }
             
             #review-name:focus {
@@ -196,19 +243,36 @@ class ReviewAdder extends HTMLElement {
         return `<div id="wrapper">
                     <h1>Add review</h1>
                     <div id="preview-wrapper">
-                        <div id="preview-signature">By <span id="preview-name">Your name</span><span id="preview-date"></span></div>
-                        <div>
+                        <div id="preview-head">
+                            <span id="preview-signature">
+                                By <i id="preview-name">${this.placeholders.name}</i> <span id="preview-date"></span>
+                            </span>
+                            <span hidden>
+                                <i class="review-star blank"></i>
+                                <i class="review-star blank"></i>
+                                <i class="review-star blank"></i>
+                                <i class="review-star blank"></i>
+                                <i class="review-star blank"></i>
+                            </span>
+                        </div>
+                        <div id="preview-main">
                             <img id="preview-avatar" src="images/blank-image.png"/>
-                            <div id="preview-text">Start typing and your review text will appear here...</div>
+                            <div id="preview-text">${this.placeholders.preview}</div>
                         </div>
                         <div id="review-pane">EDIT YOUR REVIEW</div>
                     </div>
                     <div id="review-wrapper">
                         <div id="review-personalWrapper">
                             <label>Enter your name:</label>
-                            <input id="review-name"/>
+                            <input maxlength="100" id="review-name"/>
                             <label>Choose your avatar:</label>
-                            <input type="file"/>
+                            <div id="review-upload">
+                                <span id="review-uploadText">${this.placeholders.imageNotSelected}</span>
+                                <label for="review-uploadImage">
+                                    <img src="images/upload-icon.png"/>
+                                </label>
+                                <input id="review-uploadImage" hidden type="file" accept="image/*"/>
+                            </div>
                         </div>
                         <div id="review-mainWrapper">
                             <div>
@@ -225,17 +289,17 @@ class ReviewAdder extends HTMLElement {
                                         </span>
                                     </div>
                                 </div>
-                                <textarea></textarea>
+                                <textarea id="review-text""></textarea>
                             </div>
                             <div id="review-buttons">
                                 <div>
-                                    <button class="secondary">Bold</button>
-                                    <button class="secondary">Emphasize</button>
-                                    <button class="secondary">Quote</button>
+                                    <button id="review-bold" class="secondary">Bold</button>
+                                    <button id="review-emphasize" class="secondary">Emphasize</button>
+                                    <button id="review-quote" class="secondary">Quote</button>
                                 </div>
                                 <div id="review-confirmButtons">
-                                    <button class="secondary">Cancel</button>
-                                    <button>Add review</button>
+                                    <button id="review-cancel" class="secondary">Cancel</button>
+                                    <button id="review-submit" type="submit">Add review</button>
                                 </div>
                             </div>
                         </div>
@@ -243,5 +307,75 @@ class ReviewAdder extends HTMLElement {
                 </div>`
     }
 
+    connectedCallback() {
+        this.addDate();
+        this.addReviewChangeHandler();
+        this.addImageChangeHandler();
+        this.addNameChangeHandler();
+        this.addTextFormatHandlers();
+        this.addConfirmButtonsHandlers();
+    }
+
+    addDate() {
+        const today = new Date();
+        this.shadow.getElementById('preview-date').textContent =
+            `${this.placeholders.months[today.getMonth()]} ${today.getDay()}, ${today.getFullYear()}`
+    }
+
+    addReviewChangeHandler() {
+        this.shadow.getElementById('review-text')
+            .addEventListener('keyup', e => this.forcePreviewTextUpdate(e.target.value));
+    }
+
+    forcePreviewTextUpdate(value) {
+        this.shadow.getElementById('preview-text').innerHTML = value ? value : this.placeholders.preview;
+    }
+
+    addImageChangeHandler() {
+        this.shadow.getElementById('review-uploadImage').addEventListener('change', e => {
+            if (!e.target.files[0]) return;
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                this.shadow.querySelector('#review-upload img').src = reader.result;
+                this.shadow.getElementById("preview-avatar").src = reader.result;
+            };
+            reader.readAsDataURL(e.target.files[0]);
+            const imageUploadedText = this.shadow.getElementById('review-uploadText');
+            imageUploadedText.className += ' uploaded';
+            imageUploadedText.textContent = this.placeholders.imageSelected;
+        });
+    };
+
+    addNameChangeHandler() {
+        this.shadow.getElementById('review-name').addEventListener('keyup', e =>
+            this.shadow.getElementById('preview-name').textContent =
+                e.target.value ? e.target.value : this.placeholders.name)
+    }
+
+    addTextFormatHandlers() {
+        const modifier = escaper => {
+            return () => {
+                const textarea = this.shadow.getElementById('review-text');
+                const original = textarea.value;
+                const start = textarea.selectionStart;
+                const end = textarea.selectionEnd;
+                const selection = original.substring(start, end);
+                if (!selection) return;
+                const beforeSelection = original.substring(0, start);
+                const afterSelection = original.substring(end, original.length);
+                textarea.value = `${beforeSelection}[${escaper}]${selection}[/${escaper}]${afterSelection}`;
+                this.forcePreviewTextUpdate(textarea.value);
+            }
+        };
+        this.shadow.getElementById('review-bold').addEventListener('click', modifier('b'));
+        this.shadow.getElementById('review-emphasize').addEventListener('click', modifier('i'));
+        this.shadow.getElementById('review-quote').addEventListener('click', modifier('q'));
+    }
+
+    addConfirmButtonsHandlers() {
+        this.shadow.getElementById('review-submit').addEventListener('click', e => alert(this.placeholders.submit));
+        // #todo handle cancel
+    }
 }
+
 customElements.define('review-adder', ReviewAdder);
